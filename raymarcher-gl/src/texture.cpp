7f433e32ffee46fc61a6bb1a3cb28407423e0580
Mirco Müller <macslow@gmail.com>
2018-04-11 17:17:54 +0200

    Changed texture edge-behaviour to repeat for the terrain-examples.

--- raymarcher-gl/src/texture.cpp
+++ raymarcher-gl/src/texture.cpp
@@ -99,8 +99,8 @@ GLuint Texture::loadTexture (const char* filename,
     glTexParameteri (GL_TEXTURE_2D,
                      GL_TEXTURE_MIN_FILTER,
                      GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glGenerateMipmap (GL_TEXTURE_2D);
     glBindTexture (GL_TEXTURE_2D, 0);
 
