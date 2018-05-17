eba554694adec3ca88a283328d8de27753e8eeb8
Mirco Müller <macslow@gmail.com>
2018-05-17 09:54:34 +0200

    Depth-buffer and multisampling are not really needed for the OpenGL-context of a raymarcher.

--- raymarcher-gl/src/display.cpp
+++ raymarcher-gl/src/display.cpp
@@ -80,9 +80,7 @@ Display::Display (const char* shaderfile, unsigned int width,
     SDL_GL_SetAttribute (SDL_GL_RED_SIZE, 8);
     SDL_GL_SetAttribute (SDL_GL_GREEN_SIZE, 8);
     SDL_GL_SetAttribute (SDL_GL_BLUE_SIZE, 8);
-    SDL_GL_SetAttribute (SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute (SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute (SDL_GL_MULTISAMPLESAMPLES, 4);
     SDL_GL_SetAttribute (SDL_GL_CONTEXT_PROFILE_MASK,
 						 SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute (SDL_GL_CONTEXT_PROFILE_MASK,
